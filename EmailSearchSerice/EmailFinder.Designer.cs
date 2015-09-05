377d6e14d45b18584083ff1f728aa1074d6afe67
Luke Vulpa <lukie@subvulpa.net>
2015-09-05 10:17:18 +0100

    Initial commit

---
+++ EmailSearchSerice/EmailFinder.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace EmailSearchSerice
+{
+    partial class EmailFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.emailSearchHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.exportAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(284, 51);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(193, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(27, 52);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(236, 22);
+            this.searchTextBox.TabIndex = 1;
+            this.searchTextBox.TextChanged += new System.EventHandler(this.emailTextBox_TextChanged);
+            this.searchTextBox.Enter += new System.EventHandler(this.searchTextBox_Enter);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(27, 99);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultTextBox.Size = new System.Drawing.Size(450, 302);
+            this.resultTextBox.TabIndex = 3;
+            this.resultTextBox.TextChanged += new System.EventHandler(this.resultTextBox_TextChanged);
+            this.resultTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.resultTextBox_KeyDown);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(284, 474);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(193, 27);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Export as...";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(504, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exportAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(123, 24);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.emailSearchHelpToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // emailSearchHelpToolStripMenuItem
+            // 
+            this.emailSearchHelpToolStripMenuItem.Name = "emailSearchHelpToolStripMenuItem";
+            this.emailSearchHelpToolStripMenuItem.Size = new System.Drawing.Size(208, 24);
+            this.emailSearchHelpToolStripMenuItem.Text = "Email Search Help...";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(24, 434);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(63, 17);
+            this.resultLabel.TabIndex = 5;
+            this.resultLabel.Text = "Results: ";
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Enabled = false;
+            this.pauseButton.Location = new System.Drawing.Point(284, 428);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 6;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(402, 428);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // exportAsToolStripMenuItem
+            // 
+            this.exportAsToolStripMenuItem.Name = "exportAsToolStripMenuItem";
+            this.exportAsToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.exportAsToolStripMenuItem.Text = "Export As...";
+            this.exportAsToolStripMenuItem.Click += new System.EventHandler(this.exportAsToolStripMenuItem_Click);
+            // 
+            // EmailFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 531);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "EmailFinder";
+            this.Text = "Email Search ";
+            this.Load += new System.EventHandler(this.EmailFinder_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem emailSearchHelpToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.ToolStripMenuItem exportAsToolStripMenuItem;
+    }
+}
+
---
+++ EmailSearchSerice/EmailFinder.cs
@@ -0,0 +1,427 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailSearchSerice
+{
+    //First Entry issues seems to be wrong term is adding an incorrect email first
+    public partial class EmailFinder : Form
+    {
+        private string currentLine;
+        private string aFilePath;
+        private string searchTerm;
+        delegate void SetTextCallback();
+
+        private bool searchCompleted;
+        private bool searchPaused;
+        private bool searchStopped;
+
+        StreamReader aReader;
+        StreamWriter aWriter;
+
+        Thread thread;
+
+        List<string> emailList;
+
+        public EmailFinder()
+        {
+            emailList = new List<string>();
+
+            InitializeComponent();
+            initialiseGui();
+
+            searchPaused = false;
+            searchStopped = false;
+            searchButton.Enabled = false;
+        }
+
+        #region Dialogs
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+            
+        }
+        #endregion
+
+        #region Button Click Events
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            startSearchState();
+            //sets application state
+            if (searchTerm == "")
+            {
+                searchButton.Enabled = false;
+                MessageBox.Show("Please enter a search term", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                searchTerm = searchTextBox.Text;
+                if (backgroundWorker1.IsBusy != true)
+                {
+
+                    backgroundWorker1.RunWorkerAsync();
+                }
+
+            }
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            pauseToggle();
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        exportAsTxt();
+                        break;
+                    case 2:
+                        exportAsCvs();
+                        break;
+                    case 3:
+                        exportAsHtml();
+                        break;
+                }
+            }
+            //foreach value in list add to file then close and clearlist
+            Refresh();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            this.searchStopped = true;
+            // Cancel the asynchronous operations. 
+            if (backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+            resetSearchControls();
+
+        }
+
+        #endregion
+
+        #region Other Events
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                aFilePath = openFileDialog1.FileName;
+            }
+            //searchTerm = searchTextBox.Text;
+        }
+
+        private void resultTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == (Keys.Control | Keys.A))
+            {
+                resultTextBox.SelectAll();
+                e.Handled = e.SuppressKeyPress = true;
+            }
+        }
+
+        private void emailTextBox_TextChanged(object sender, EventArgs e)
+        {
+            searchTerm = searchTextBox.Text;
+            if (searchTextBox.Text != "")
+            {
+                searchButton.Enabled = true;
+            }
+            else
+            {
+                searchButton.Enabled = false;
+            }
+        }
+
+        private void EmailFinder_Load(object sender, EventArgs e)
+        {
+            aReader.Close();
+        }
+
+        #endregion
+
+        #region Gui States
+
+        private void startSearchState()
+        {
+            //searchTerm = searchTextBox.Text;
+            emailList = new List<string>();
+            
+            pauseButton.Visible = true;
+            pauseButton.Text = "Pause";
+            pauseButton.Enabled = true;
+
+            searchPaused = false;
+            searchStopped = false;
+            searchButton.Enabled = false;
+            searchTextBox.Enabled = false;
+
+            stopButton.Visible = true;
+            stopButton.Enabled = true;
+
+            resultTextBox.Clear();
+
+            resultLabel.Text = "Results: " + emailList.Count;
+        }
+
+        void resetSearchControls()
+        {
+            //search clicked
+            // change button and textbox state
+
+            //searchTerm = searchTextBox.Text;
+            pauseButton.Visible = false;
+            pauseButton.Text = "Pause";
+            
+            searchTextBox.Enabled = true;
+            searchButton.Enabled = false;
+
+            stopButton.Visible = false;
+            saveButton.Visible = false;
+
+            if(emailList.Count >= 1)
+            {
+                resultTextBox.Enabled = true;
+            }
+            else
+            {
+
+                resultTextBox.Enabled = false;
+                
+            }
+
+            if (searchStopped || searchCompleted)
+            {
+                searchButton.Enabled = false;
+                
+                stopButton.Visible = false;
+                searchTextBox.Clear();
+                saveButton.Visible = true;
+                this.searchPaused = false;
+
+                saveFileDialog1.FileName = searchTerm;
+            }
+
+
+            this.Refresh();
+
+        }
+
+        private void initialiseGui()
+        {
+            openFileDialog1.Filter = "Text File|*.txt|Cvs File|*.cvs|Html File|*.html";
+            saveFileDialog1.Filter = "Text File|*.txt|Cvs File|*.cvs|Html File|*.html";
+
+            searchTerm = "";
+            searchTextBox.Text = searchTerm;
+            searchButton.Enabled = false;
+
+            resultLabel.Text = "Results: " + emailList.Count;
+            resultTextBox.Enabled = false;
+
+            saveButton.Visible = false;
+            pauseButton.Visible = false;
+            stopButton.Visible = false;
+        }
+
+        #endregion
+        
+        private void pauseToggle()
+        {
+            searchPaused = !searchPaused;
+
+            if (searchPaused)
+            {
+                pauseButton.Text = "Resume";
+            }
+            else
+            {
+                pauseButton.Text = "Pause";
+            }
+        }
+
+        #region BackgroundWorker
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            thread = new Thread(new ThreadStart(proc));
+ 
+
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+            else
+            {
+                try
+                {
+                    aReader = new StreamReader(aFilePath);
+                    
+                    while (!aReader.EndOfStream || aReader != null)
+                    {
+                        if (searchPaused != true && searchStopped != true)
+                        {                            
+                            currentLine = aReader.ReadLine();
+
+                            if (currentLine.EndsWith(searchTerm, StringComparison.CurrentCulture))
+                            {
+                                proc();
+                            }
+                        }
+                        if (searchStopped)
+                        {
+                            backgroundWorker1.CancelAsync();
+                        }
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+                    MessageBox.Show("Invalid file or file not selected", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                catch (NullReferenceException)
+                {
+                    MessageBox.Show("End of file reached", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    searchCompleted = true;
+                    
+                }
+                finally
+                {
+                    backgroundWorker1.CancelAsync();
+                }
+            }
+        }
+
+        private void proc()
+        {
+            
+            if (resultTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(proc);
+                this.Invoke(d, new object[] { });
+                resultTextBox.Enabled = true;
+            }
+            else
+            {
+                resultLabel.Text = "Results: " + emailList.Count;
+                resultTextBox.Enabled = true;
+                resultTextBox.AppendText(this.currentLine + Environment.NewLine);
+                emailList.Add(currentLine);
+                
+            }
+        }
+
+        //private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        //{
+        //    progressBar1.Value = e.ProgressPercentage;
+
+        //}
+
+        //when backgroundWork has finish- or error - cancelled
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            resetSearchControls();
+            searchTextBox.Clear();
+        }
+
+
+        #endregion
+
+        private void searchTextBox_Enter(object sender, EventArgs e)
+        {
+            if(searchTextBox.Text != "")
+            {
+                searchButton.Enabled = true;
+            }
+            else
+            {
+                searchButton.Enabled = false;
+            }
+        }
+
+        private void resultTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exportAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+            exportButton_Click(sender,e);
+
+        }
+
+        void exportAsCvs()
+        {
+           
+            aWriter = new StreamWriter(saveFileDialog1.FileName);
+
+            foreach(string element in emailList)
+            {
+                aWriter.WriteLine(element + "," );
+
+            }
+
+            aWriter.Close();
+        
+        }
+
+        void exportAsTxt()
+        {
+            aWriter = new StreamWriter(saveFileDialog1.FileName);
+
+            foreach (string element in emailList)
+            {
+                aWriter.WriteLine(element);
+            }
+
+            aWriter.Close();
+        
+        }
+
+        void exportAsHtml()
+        {
+            aWriter = new StreamWriter(saveFileDialog1.FileName);
+
+            aWriter.WriteLine("<!DOCTYPE html><html><head>");
+            aWriter.WriteLine("<title>Title of the document</title></head>");
+            aWriter.WriteLine("<style> </style>");
+            aWriter.WriteLine("<body><table style=\"width:100%\"/><tr><td>Search results for" + searchTerm + "</td>");
+            
+            foreach (string element in emailList)
+            {
+                aWriter.WriteLine("<td>" + element + "</td>");
+
+            }
+            aWriter.WriteLine("</tr></table></body></html>");
+
+            aWriter.Close();
+        }
+
+
+    }
+}
